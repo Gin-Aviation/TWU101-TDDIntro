bf86654c3d39bca76044d3b50f5155b1591f3c09
HangZhao <327147195@qq.com>
2020-02-19 22:50:12 +0800

    finsih AccountTest

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,30 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+    private int money;
+
+    public Account(){
+        money = 0;
+    }
+    public Account(int newMoney){
+        money = newMoney;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public void deposit(int depositMoney){
+        money = depositMoney + money;
+    }
+
+    public void withdraw(int withdrawMoney){
+        if(money >= withdrawMoney) {
+            money = money - withdrawMoney;
+        }
+    }
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -3,22 +3,33 @@ package com.thoughtworks.tddintro.exercises.accountbalance;
 import org.junit.Ignore;
 import org.junit.Test;
 
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class AccountTests {
+    private final Account account = new Account(50);
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-        Account account = new Account();
+        int originMoney = account.getMoney();
+        account.deposit(50);
+        assertThat(account.getMoney(),is(originMoney + 50));
+
     }
 
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
+        int originMoney = account.getMoney();
+        account.withdraw(50);
+        assertThat(account.getMoney(),is(originMoney - 50));
 
     }
 
     @Test
-    @Ignore  // Remove each @Ignore and implement test
-    public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
 
+    public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
+        int originMoney = account.getMoney();
+        account.withdraw(100);
+        assertThat(account.getMoney(),is(originMoney));
     }
 }
